71cd940b369a79a205812ebae7d750f48abec6e2
Roland Branten <roland.branten@gmail.com>
2020-04-12 11:10:54 +0200

    initial commit

---
+++ dashboardConfig.js
@@ -0,0 +1,34 @@
+export default {
+  widgets: [
+    {
+      name: "project-info"
+    },
+    {
+      name: "project-users"
+    }
+    // {
+    //   name: "netlify",
+    //   options: {
+    //     title: "Netlify",
+    //     sites: [
+    //       {
+    //         title: "TITLE",
+    //         apiId: env.id,
+    //         buildHookId: env.hook,
+    //         name: "NAME"
+    //       }
+    //     ]
+    //   }
+    // },
+    // {
+    //   name: "gatsby",
+    //   options: {
+    //     sites: [
+    //       {
+    //         siteUrl: "URL"
+    //       }
+    //     ]
+    //   }
+    // }
+  ]
+};
